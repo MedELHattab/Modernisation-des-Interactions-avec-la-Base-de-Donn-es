f3247d3af4215da03d3cb51861816e6068e9eeba
MedELHattab <elhattabmohammedelarbi.com>
2023-12-15 12:40:07 +0100

    I just add an example of how dynamic autoload with composer

---
+++ classes/class1.php
@@ -0,0 +1,7 @@
+<?php
+namespace classes;
+
+class Class1 {
+    public function echoHello() {
+        echo"hello";
+    }};
---
+++ fxx.php
@@ -0,0 +1,6 @@
+<?php
+use classes\Class1;
+require ("./vendor/autoload.php");
+$obj = new Class1();
+
+$obj->echoHello();
